5f7d47681492c8ed46c6762975205f56e1e07fbd
wapiti08 <tzrzhuoran@163.com>
2018-06-06 14:36:52 +0800

    Add files via upload

---
+++ bub.py
@@ -0,0 +1,38 @@
+import numpy as np
+import matplotlib.pyplot as mp
+import matplotlib.animation as ma
+import random
+
+n_samples=100
+#开始时所有的值用0来填
+#其中2是两列的意思
+data=np.zeros(n_samples,dtype=[
+    ('position',float,2),
+    ('size',float,1),
+    ('growth',float,1),
+    ('color',float,4)])
+#R.uniform(a,b)	返回[a,b) 区间内的随机实数
+data['position']=np.random.uniform(0,1,(n_samples,2))
+data['size']=np.random.uniform(50,750,n_samples)
+data['growth']=np.random.uniform(30,150,n_samples)
+data['color']=np.random.uniform(0,1,(n_samples,4))
+mp.figure('Bubble',facecolor='lightgray')
+mp.title('Bubble',fontsize=20)
+mp.xlabel('x',fontsize=14)
+mp.ylabel('y',fontsize=14)
+mp.tick_params(labelsize=10)
+mp.grid(linestyle=":")
+plot=mp.scatter(data['position'][:,0],data['position'][:,1],s=data['size'],c=data['color'])
+
+#gcf获取当前图
+
+#接收返回值以后就会变成进程级的返回值
+def update(number):
+    data['size']+=data['growth']
+    plot.set_sizes(data['size'])
+
+#一方面要变，同时还要把变化的值告诉函数
+#需要接收返回值，不然返回值会随语句的执行而消失
+anim=ma.FuncAnimation(mp.gcf(),update,interval=10)
+mp.show()
+
